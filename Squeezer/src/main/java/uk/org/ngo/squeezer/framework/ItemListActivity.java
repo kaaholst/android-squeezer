2f4049e16f37c14e21851daa9fd02b874508a95f
Kurt Aaholst <kaaholst@gmail.com>
2025-02-13 23:49:59 +0100

    Bottom bar color

--- Squeezer/src/main/java/uk/org/ngo/squeezer/framework/ItemListActivity.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/framework/ItemListActivity.java
@@ -17,6 +17,7 @@
 package uk.org.ngo.squeezer.framework;
 
 
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -156,6 +157,9 @@ public abstract class ItemListActivity extends BaseActivity implements ItemAdapt
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            getWindow().setNavigationBarContrastEnforced(false);
+        }
 
         mPageSize = getResources().getInteger(R.integer.PageSize);
 
