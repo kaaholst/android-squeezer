f53e9b509a7283d74cbea5c9e77612eeab6b304e
Kurt Aaholst <kaaholst@gmail.com>
2025-09-30 08:16:43 +0200

    In about dialog add Link to website and move license to website

--- Squeezer/src/main/java/uk/org/ngo/squeezer/dialog/AboutDialog.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/dialog/AboutDialog.java
@@ -18,10 +18,11 @@ package uk.org.ngo.squeezer.dialog;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -32,7 +33,6 @@ import android.widget.TextView;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
-import uk.org.ngo.squeezer.BuildConfig;
 import uk.org.ngo.squeezer.R;
 import uk.org.ngo.squeezer.Squeezer;
 
@@ -44,10 +44,9 @@ public class AboutDialog extends DialogFragment {
         final View view = getActivity().getLayoutInflater().inflate(R.layout.about_dialog, null);
         final TextView titleText = view.findViewById(R.id.about_title);
         final TextView versionText = view.findViewById(R.id.version_text);
-        final TextView support = view.findViewById(R.id.support);
-        final TextView privacy = view.findViewById(R.id.privacy);
-        support.setMovementMethod(LinkMovementMethod.getInstance());
-        privacy.setMovementMethod(LinkMovementMethod.getInstance());
+        view.<TextView>findViewById(R.id.website).setMovementMethod(LinkMovementMethod.getInstance());
+        view.<TextView>findViewById(R.id.issues).setMovementMethod(LinkMovementMethod.getInstance());
+        view.<TextView>findViewById(R.id.privacy).setMovementMethod(LinkMovementMethod.getInstance());
 
         PackageManager pm = getActivity().getPackageManager();
         PackageInfo info;
@@ -61,20 +60,11 @@ public class AboutDialog extends DialogFragment {
         MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getActivity());
         builder.setView(view);
         builder.setPositiveButton(android.R.string.ok, null);
-        builder.setNeutralButton(R.string.changelog_full_title, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                ChangeLogDialog changeLog = new ChangeLogDialog(getActivity(), Squeezer.getPreferences().getSharedPreferences());
-                changeLog.getThemedFullLogDialog().show();
-            }
+        builder.setNeutralButton(R.string.changelog_full_title, (dialog, which) -> {
+            ChangeLogDialog changeLog = new ChangeLogDialog(getActivity(), Squeezer.getPreferences().getSharedPreferences());
+            changeLog.getThemedFullLogDialog().show();
         });
-        builder.setNegativeButton(R.string.dialog_license, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                new LicenseDialog()
-                        .show(getActivity().getSupportFragmentManager(), "LicenseDialog");
-            }
-        });
+        builder.setNegativeButton(R.string.dialog_license, (dialog, which) -> requireActivity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.license)))));
         return builder.create();
     }
 }
