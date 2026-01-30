998f57b064bfeda19d2bfe0feb2d83ec3d436a59
David Smith <dave@smithfamily.org.uk>
2026-01-30 16:10:03 +0000

    Add the currently playing track to the screensaver

--- Squeezer/src/main/java/uk/org/ngo/squeezer/screensaver/Screensaver.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/screensaver/Screensaver.java
@@ -15,10 +15,20 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.TextClock;
+import android.widget.TextView;
 
 import java.util.Random;
 
 import uk.org.ngo.squeezer.R;
+import uk.org.ngo.squeezer.Squeezer;
+import uk.org.ngo.squeezer.SqueezerRepository;
+import uk.org.ngo.squeezer.model.CurrentTrack;
+import uk.org.ngo.squeezer.model.Player;
+import uk.org.ngo.squeezer.model.PlayerState;
+import uk.org.ngo.squeezer.service.event.ActivePlayerChanged;
+import uk.org.ngo.squeezer.service.event.MusicChanged;
+import uk.org.ngo.squeezer.service.event.PlayStatusChanged;
+import uk.org.ngo.squeezer.service.event.PowerStatusChanged;
 
 public class Screensaver extends AppCompatActivity {
     private static final long MOVE_INTERVAL = 10 * 60 * 1000; // 10 minutes
@@ -28,6 +38,8 @@ public class Screensaver extends AppCompatActivity {
     private final Random random = new Random();
     private View clockContainer;
     private ViewGroup screensaverContainer;
+    private TextView trackView;
+    private Player activePlayer;
 
     private final Runnable moveRunnable = new Runnable() {
         @Override
@@ -53,13 +65,62 @@ public class Screensaver extends AppCompatActivity {
 
         screensaverContainer = findViewById(R.id.screensaver_container);
         clockContainer = findViewById(R.id.clock_container);
+        trackView = findViewById(R.id.track);
 
         TextClock date = findViewById(R.id.date);
         String pattern = DateFormat.getBestDateTimePattern(getResources().getConfiguration().locale, "EdMMM");
         date.setFormat12Hour(pattern);
         date.setFormat24Hour(pattern);
 
+        SqueezerRepository repository = ((Squeezer) getApplicationContext()).repository();
+        repository.observe(this, (ActivePlayerChanged event) -> {
+            activePlayer = event.player;
+            updateTrack(trackView, activePlayer);
+        });
+        repository.observe(this, (MusicChanged event) -> {
+            if (activePlayer != null && activePlayer.equals(event.player)) {
+                activePlayer = event.player;
+                updateTrack(trackView, activePlayer);
+            }
+        });
+        repository.observe(this, (PlayStatusChanged event) -> {
+            if (activePlayer != null && activePlayer.equals(event.player)) {
+                activePlayer = event.player;
+                updateTrack(trackView, activePlayer);
+            }
+        });
+        repository.observe(this, (PowerStatusChanged event) -> {
+            if (activePlayer != null && activePlayer.equals(event.player)) {
+                activePlayer = event.player;
+                updateTrack(trackView, activePlayer);
+            }
+        });
+
         moveHandler.postDelayed(moveRunnable, MOVE_INTERVAL);
+    }
+
+    private void updateTrack(TextView trackView, Player player) {
+        CharSequence oldText = trackView.getText();
+        int oldVisibility = trackView.getVisibility();
+        if (player != null && player.getConnected() && player.getPlayerState().isPoweredOn()) {
+            PlayerState playerState = player.getPlayerState();
+            CurrentTrack currentTrack = playerState.getCurrentTrack();
+            if (currentTrack != null && !PlayerState.PLAY_STATE_STOP.equals(playerState.getPlayStatus())) {
+                String artist = currentTrack.songInfo.getArtist();
+                String title = currentTrack.songInfo.title;
+                String text = artist.isEmpty() ? title : artist + " - " + title;
+                trackView.setText(text);
+                trackView.setVisibility(View.VISIBLE);
+                if (!text.equals(oldText) || oldVisibility != View.VISIBLE) {
+                    clockContainer.post(this::moveClock);
+                }
+                return;
+            }
+        }
+        trackView.setVisibility(View.GONE);
+        if (oldVisibility != View.GONE) {
+            clockContainer.post(this::moveClock);
+        }
     }
 
     private void moveClock() {
