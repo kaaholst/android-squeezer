6d887cb955622dae19df5e091f139f0fe3754336
Thomas Malcher <thomas.malcher@gmail.com>
2026-07-20 09:43:07 +0200

    Generalize AlarmPlaylistActivity to select by URL instead of Alarm
    Decouples the playlist picker from the Alarm model so it can be reused
    by other callers that need to pick a playlist/favorite by URL, such as
    the upcoming preset editor.

--- Squeezer/src/main/java/uk/org/ngo/squeezer/itemlist/AlarmPlaylistAdapter.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/itemlist/AlarmPlaylistAdapter.java
@@ -16,17 +16,16 @@ import java.util.List;
 import uk.org.ngo.squeezer.R;
 import uk.org.ngo.squeezer.framework.BaseActivity;
 import uk.org.ngo.squeezer.framework.ItemViewHolder;
-import uk.org.ngo.squeezer.model.Alarm;
 import uk.org.ngo.squeezer.model.AlarmPlaylist;
 
 class AlarmPlaylistAdapter extends RecyclerView.Adapter<ItemViewHolder<AlarmPlaylist>> {
     private final BaseActivity activity;
-    private final Alarm alarm;
+    private final String currentUrl;
     private List<AlarmPlaylist> playlists;
 
-    public AlarmPlaylistAdapter(BaseActivity activity, Alarm alarm) {
+    public AlarmPlaylistAdapter(BaseActivity activity, String currentUrl) {
         this.activity = activity;
-        this.alarm = alarm;
+        this.currentUrl = currentUrl;
     }
 
     @Override
@@ -60,7 +59,7 @@ class AlarmPlaylistAdapter extends RecyclerView.Adapter<ItemViewHolder<AlarmPlay
         public void bindView(AlarmPlaylist item) {
             super.bindView(item);
             ((TextView) itemView).setText(item.getName());
-            @AttrRes int background = (item.getId().equals(alarm.getPlayListId())) ? R.attr.currentTrackBackground : R.attr.selectableItemBackground;
+            @AttrRes int background = (item.getId().equals(currentUrl)) ? R.attr.currentTrackBackground : R.attr.selectableItemBackground;
             itemView.setBackgroundResource(getActivity().getAttributeValue(background));
             itemView.setOnClickListener(v -> {
                 Intent intent = new Intent().putExtra(AlarmPlaylistActivity.ALARM_PLAYLIST, item);
